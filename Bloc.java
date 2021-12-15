e886c79adb2d416b0664366e8c75de315fe4e66d
Eladio05 <eliottp59@sfr.fr>
2021-12-15 10:47:39 +0100

    Classe Bloc

---
+++ Bloc.java
@@ -0,0 +1,39 @@
+
+public class Bloc {
+	private String Couleur;
+	private int taille;
+	
+	
+	
+	public Bloc(String couleur, int taille) {
+		super();
+		Couleur = couleur;
+		this.taille = taille;
+	}
+	
+	
+	public String getCouleur() {
+		return Couleur;
+	}
+	public void setCouleur(String couleur) {
+		Couleur = couleur;
+	}
+	public int getTaille() {
+		return taille;
+	}
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Bloc Couleur=" + Couleur + ", taille=" + taille;
+	}
+	
+	
+	
+	
+	
+	
+}
