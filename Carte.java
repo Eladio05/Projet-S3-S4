15c2dfe0e9fc6e40ecdc776e1ee9cc9808c7d164
Eladio05 <eliottp59@sfr.fr>
2021-12-15 10:52:30 +0100

    Classe Carte

---
+++ Carte.java
@@ -0,0 +1,44 @@
+
+public class Carte {
+	private int abscisse;
+	private int ordonne;
+	
+	public Carte(int abscisse, int ordonne) {
+		super();
+		this.abscisse = abscisse;
+		this.ordonne = ordonne;
+	}
+	
+	
+	
+	
+
+	@Override
+	public String toString() {
+		return "Carte abscisse=" + abscisse + ", ordonne=" + ordonne;
+	}
+
+
+
+
+
+	public int getAbscisse() {
+		return abscisse;
+	}
+
+	public void setAbscisse(int abscisse) {
+		this.abscisse = abscisse;
+	}
+
+	public int getOrdonne() {
+		return ordonne;
+	}
+
+	public void setOrdonne(int ordonne) {
+		this.ordonne = ordonne;
+	}
+	
+	
+	
+	
+}
