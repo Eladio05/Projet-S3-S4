847ed05c64cd8b166b31637b1349b60aadcf8fb1
Guaros <enzo02260@gmail.com>
2022-01-31 13:56:14 +0100

    initialiser joueurs

--- Model/Partie.java
+++ Model/Partie.java
@@ -14,11 +14,10 @@ public class Partie
 	public Partie()
 	{
 		this.m = new Map();	
-		
 		this.listeJoueurs = new ArrayList<>();
 		
 		this.initialiserPioche();
-		//this.initialiserJoueurs();
+		this.initialiserJoueurs();
 	}
 
 	private void initialiserPioche()
@@ -42,9 +41,6 @@ public class Partie
 	
 	private void melangerPioche()
 	{
-		System.out.println(this.pioche);
-		
-		
 		Random r = new Random();
 		for (int i=0 ; i < 2000 ; i=i+1)
 		{
@@ -56,23 +52,51 @@ public class Partie
 			this.pioche.getListCarte().set(indiceC1, this.pioche.getListCarte().get(indiceC2));
 			this.pioche.getListCarte().set(indiceC2, c1);
 		}
-		
-		System.out.println(this.pioche);
 	}
 	
-	/*
-	0-0 : IIIII
-	0-1 : IIIII
-	1-0 : IIIII
-	1-1 : IIIII
-	 */
-	
-	
-	
-	/*
 	private void initialiserJoueurs()
 	{
 		
+		int nbJoueurs = new Scanner(System.in).nextInt();
+		for (int i=0 ; i<nbJoueurs ; i=i+1)
+		{
+			String pseudo = new Scanner(System.in).nextLine();
+			String couleur = "";
+			
+			
+			if (i == 0)
+			{
+				couleur = "Jaune";
+			}
+			else if (i == 1)
+			{
+				couleur = "Violet";
+			}
+			else if ( i==2 )
+			{
+				couleur = "Bleu";
+			}
+			else 
+			{
+				couleur = "Orange";
+			}
+			
+			DeckBlocs db = new DeckBlocs(couleur);
+			ArrayList<Carte> lc = new ArrayList<>();
+			
+			for (int c=0 ; c<4 ; c=c+1)
+			{
+				lc.add(c, this.pioche.getListCarte().get(c));
+				this.pioche.getListCarte().remove(c);
+			}
+			
+			DeckCarte dc = new DeckCarte(lc);
+			Joueur j = new Joueur(pseudo, dc, db);
+		}
+		
+		
+		
 	}
-	*/
+
+	
 }
